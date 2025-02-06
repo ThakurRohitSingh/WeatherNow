6daebb2d9917e9376cce1d5e8cbac012f2b77298
Rohit Singh Thakur <thakurrohitsingh04@gmail.com>
2025-02-06 23:33:03 +0530

    Refactor

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,10 @@
-import { StrictMode } from 'react'
+import React from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-// import FetchData from './FetchData.jsx'
 
 createRoot(document.getElementById('root')).render(
-  // <StrictMode>
+    <>
     <App />
-    // <FetchData/>
-  // </StrictMode>,
+    </>
 )

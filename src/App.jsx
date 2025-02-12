e774d4bed3bb33194322936ed97dcd71e7ead530
Rohit Singh Thakur <thakurrohitsingh04@gmail.com>
2025-02-12 21:55:17 +0530

    Add button functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,43 @@
-import React, { useEffect, useState } from 'react'
-import './App.css'
+import React, { useEffect, useState } from 'react';
+import './App.css';
 import DailyForecast from './Components/DailyForecast';
 import WeeklyForecast from './Components/WeeklyForecast';
 import AirCondition from './Components/AirCondition';
 import WeatherInfo from './Components/WeatherInfo';
 import { fetchWeatherData } from './FetchWeather';
 
-
 const App = () => {
 
   const [city, setCity] = useState("");
   const [weatherData, setWeatherData] = useState();
 
-  useEffect(() => {
-    const getWeather = async () => {
-      if (!city.trim()) return; // Prevents API call if city is empty
-      const data = await fetchWeatherData(city);
-      setWeatherData(data);
-    };
-    getWeather();
-  }, [city]);
+  const getWeather = async () => {
+    if (!city.trim()) return; // Prevents API call if city is empty
+    const data = await fetchWeatherData(city);
+    setWeatherData(data);
+  };
 
   return (
     <section className='container'>
-      <div className="nav">
-
+      <div className="nav bg-white">
       </div>
       <div className="content">
         <div className='card1'>
-          <input type="text"
-            placeholder='Search for City'
-            id='search'
-            value={city}
-            onChange={(e) => setCity(e.target.value)} />
+          <div className="input-container">
+            <input type="text"
+              placeholder='Search for City'
+              id='search'
+              value={city}
+              onChange={(e) => setCity(e.target.value)} />
+            <button onClick={getWeather}>Search</button>
+          </div>
 
           {
             weatherData && (
               <>
                 <WeatherInfo data={weatherData} />
                 <DailyForecast data={weatherData.forecast.forecastday[0].hour} />
-                <AirCondition data={weatherData}/>
+                <AirCondition data={weatherData} />
               </>
             )
           }
@@ -49,7 +47,7 @@ const App = () => {
         }
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default App
+export default App;
